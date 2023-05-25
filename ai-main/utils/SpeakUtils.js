c0d7d4fab0d4640cf451170337c30c0e008f7530
Kerberos <antoniokerbey@gmail.com>
2023-05-25 11:45:29 +0800

    final

---
+++ ai-main/utils/SpeakUtils.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class SpeakUtils {
+  constructor() {}
+
+  speak(sentence) {
+    document
+      .querySelector('.stop-generating')
+      .classList.remove('hidden');
+
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+
+    const n = sentence.length * .65;
+
+    setTimeout(() => {
+      document
+        .querySelector('.stop-generating')
+        .classList.add('hidden');
+    }, n * 50);
+  }
+}
+
+export default SpeakUtils;
